8fbda3b248609ed09e129849a0f1413bdc7f5bdb
NDTrung <ndtrung1998@gmail.com>
2024-06-29 17:31:17 +0700

    feat: all test pased, no styling, no readme yet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const defaultInput = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
+const defaultOutput = "";
 
 function App() {
+  const [input, setInput] = useState(defaultInput);
+  const [output, setOutput] = useState(defaultOutput);
+  const [loadedScript, setLoadedScript] = useState(false);
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.setAttribute("crossorigin", "anonymous");
+    script.src =
+      "https://cdnjs.cloudflare.com/ajax/libs/marked/13.0.1/marked.min.js";
+    script.integrity =
+      "sha512-aQeokRCySoYEf2Ii3HlYvRHZ7qCfL/9M4EhawTlxYr3OKhZHUA7WxyVBPVHypYEdO33CjD8UhwxzP2tQsI7yrw==";
+    script.setAttribute("referrerpolicy", "no-referrer");
+    script.addEventListener("load", () => {
+      setLoadedScript(true);
+    });
+    document.head.appendChild(script);
+    
+    convert(input);
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, [loadedScript]);
+
+  function convert(input) {
+    loadedScript &&
+      window.marked.use({ breaks: true, gfm: true }) &&
+      setOutput(window.marked.parse(input));
+  }
+
+  function handleOnChange(event) {
+    setInput(() => event.target.value);
+    convert(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea id="editor" onChange={handleOnChange} value={input}></textarea>
+      <div id="preview" dangerouslySetInnerHTML={{ __html: output }}></div>
     </div>
   );
 }
